b18dc4674df0563debcdae3c2178dd71c40cd2e1
Cory <cameyer182@gmail.com>
2021-10-31 22:40:29 -0400

    commit tree d0a3a45c337600ce4ff93f9c6ddd98c99f30ffa3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end 
+
+ def kind
+    validate_triangle
+  
+      if (@length1 == @length2 && @length2 == @length3)
+        return :equilateral
+      elsif (@length1 == @length2)||(@length2 == @length3)||(@length1 == @length3)
+        return :isosceles
+      elsif (@length1 != @length2) && (@length2 != @length3) && (@length1 != @length3)
+        return :scalene 
+      end
+  end 
+
+def validate_triangle
+  valid_triangle = [(@length1 + @length2 > @length3), (@length2 + @length3 > @length1), (@length1 + @length3 > @length2)]
+  [@length1, @length2, @length3].each do |side|
+    valid_triangle << false if side <= 0 
+  raise TriangleError if valid_triangle.include?(false)
+  end
+end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end 
 end
